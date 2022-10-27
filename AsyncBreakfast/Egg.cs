02a9bde3c72da39d37b7e1c9a0f65989ab3c0e44
Mafaz Abrar Jan Chowdhury <mafazabrar@gmail.com>
2022-10-27 16:02:52 +1100

    Synchronous breakfast completed.

---
+++ AsyncBreakfast/Egg.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AsyncBreakfast
+{
+    class Egg
+    {
+        public bool fried = false;
+    }
+}
--- AsyncBreakfast/Program.cs
+++ AsyncBreakfast/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace AsyncBreakfast
 {
@@ -6,7 +7,42 @@ namespace AsyncBreakfast
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Began making breakfast.");
+
+            Egg friedEgg = FryEgg();
+            Bread toastedBread = ToastBread();
+            Tea madeTea = MakeTea();
+
+            Console.WriteLine("Breakfast ready!");
+        }
+
+
+        private static Egg FryEgg()
+        {
+            Console.WriteLine("Frying egg..");
+            Task.Delay(2000).Wait();
+            Console.WriteLine("Egg fried.");
+            Egg egg = new Egg();
+            return egg;
+        }
+
+        private static Bread ToastBread()
+        {
+            Console.WriteLine("Toasting bread..");
+            Task.Delay(2000).Wait();
+            Console.WriteLine("Bread toasted.");
+            Bread bread = new Bread();
+            return bread;
+        }
+        
+        private static Tea MakeTea()
+        {
+            Console.WriteLine("Making tea...");
+            Task.Delay(2000).Wait();
+            Console.WriteLine("Tea made.");
+
+            Tea tea = new Tea();
+            return tea;
         }
     }
 }
